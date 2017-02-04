b327069dca85ae91ff8f2fdd02a6620b41fd3210
jezierski-s <jezierski@cbbgames.com>
2017-02-04 23:32:07 +0100

    -header, footer & imports fragments added -password length validation added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 delete from user;
-insert into user(id, login, mail) values(1,'Siva', 'siva@gmail.com');
-insert into user(id, login, mail) values(2,'Prasad', 'prasad@gmail.com');
-insert into user(id, login, mail) values(3,'Reddy', 'redddy@gmail.com');
+insert into user(id, login, mail, name) values(1,'Siva', 'siva@gmail.com', '');
+insert into user(id, login, mail, name) values(2,'Prasad', 'prasad@gmail.com', '');
+insert into user(id, login, mail, name) values(3,'Reddy', 'redddy@gmail.com', '');
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -2,6 +2,12 @@ var password = document.getElementById("formPassword")
 , confirm_password = document.getElementById("formPassword2");
 
 function validatePassword(){
+  if (password.value.length < 6) {
+    password.setCustomValidity("Hasło jest za krótkie!");
+  }
+  else {
+    password.setCustomValidity('');
+  }
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Hasła są różne!");
   } else {
